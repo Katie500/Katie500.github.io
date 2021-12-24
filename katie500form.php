b6534d39ad60caa3602cd0e3107a3db39f24addd
Katie500 <56743314+Katie500@users.noreply.github.com>
2021-12-24 08:40:07 +0000

    Update katie500form.php

--- katie500form.php
+++ katie500form.php
@@ -5,41 +5,13 @@
   $message = $_POST['Message'];
 
 
-  function IsInjected($str)
-{
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-               
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-    
-    if(preg_match($inject,$str))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
+ 
 
   $email_from = 'kknauss19@gmail.com';
 
-	$email_subject = "New Form submission";
+  $email_subject = "New Form submission";
 
-	$email_body = "You have received a new message from the user $name.\n".
+  $email_body = "You have received a new message from the user $name.\n".
                             "Here is the message:\n $message".
 
 
