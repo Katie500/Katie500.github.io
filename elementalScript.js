952dea3b8e4cd81c2553f98698319c61b324d361
Katie500 <katherine.knauss@ucalgary.ca>
2023-11-06 17:53:09 -0700

    fix elements

--- elementalScript.js
+++ elementalScript.js
@@ -3,12 +3,14 @@
 // Assignment 3
 // Name: Katherine Knauss 
 // UCID: 30087243 
- 
+
+
  // JavaScript code for game logic
  const cells = document.querySelectorAll('.cell');
  let currentPlayer = '1';
  let xBorderColor = 'red'; // Border color for player 1
  let oBorderColor = 'blue'; // Border color for player 2
+ let previouslyPlayed = null;
 
  // Initialize imageClick to '1' (fire) for player 1
  let imageClick = '1';
@@ -44,26 +46,18 @@
      imageClick = '3';
  });
 
- // Define the relationships between elements
- const elementStrengths = {
-     fire: 'earth',
-     earth: 'water',
-     water: 'fire',
-     
- };
 
  function handleCellClick() {
      const cell = this; // 'this' refers to the clicked cell
          // Get the current player's element
          const currentPlayerElement = imageClick === '1' ? 'fire' : (imageClick === '2' ? 'water' : (imageClick === '3' ? 'earth' : ''));
-         if(canPlaceToken(currentPlayerElement, cell, imageClick)) {
+
+         if(canPlaceToken(currentPlayerElement, cell)) {
              cell.innerHTML = `<div class="img-container"><img src="./pictures/${currentPlayerElement}.jpg" alt="Player ${currentPlayer}" style="border: 5px solid ${currentPlayer === '1' ? xBorderColor : oBorderColor}">${currentPlayer}</div>`;
+             previouslyPlayed = cell;
 
              // Check for a winner after each move
              if (checkForWinner()) {
-                 // clearBoard();
-                 // Handle the case where a player wins
-                 // You can display a winner screen or take appropriate actions
              } else {
                  // Switch to the other player
                  currentPlayer = currentPlayer === '1' ? '2' : '1';
@@ -73,41 +67,46 @@
          }
  }
 
-
-
- function canPlaceToken(currentPlayerElement, cell, imageClick) {
+//checks if token is allowed to be played
+ function canPlaceToken(currentPlayerElement, cell) {
      const playerCell = cell.textContent; // Get the element of the cell
-     const cellImg = imageClick;
      if (playerCell === '') {
          // The cell is empty, and any token can be placed on it
          return true;
      }
      const imageElement = cell.querySelector('img');
      const source = imageElement.getAttribute('src');
-     
-     if(source === './pictures/fire.jpg') {
-         console.log(currentPlayerElement);
-         if(currentPlayerElement === 'water') {
-             return true;
+
+    //check if current cell was previously played to prevent loops
+     if(!cell === previouslyPlayed)
+     {
+        //find element weaknesses
+         if(source === './pictures/fire.jpg') {
+             if(currentPlayerElement === 'water') {
+                 return true;
+             }
+         }
+         else if(source === './pictures/water.jpg') {
+             if(currentPlayerElement === 'earth') {
+                 return true;
+             }
+         }
+         else if(source === './pictures/earth.jpg') {
+             if(currentPlayerElement === 'fire') {
+                 return true;
+             }
+         }   
+         else {
+             return false;
          }
      }
-     else if(source === './pictures/water.jpg') {
-         console.log(currentPlayerElement);
-         if(currentPlayerElement === 'earth') {
-             return true;
-         }
-     }
-     else if(source === './pictures/earth.jpg') {
-         if(currentPlayerElement === 'fire') {
-             return true;
-         }
-     }   
      else {
          return false;
      }
  }
 
  function checkForWinner() {
+    //check for these winning combingations for a 5x5 grid
      const winningCombinations = [
          [0, 1, 2, 3, 4], [5,6,7,8,9], [10, 11,12,13,14], [15,16,17,18,19],[20, 21,22,23,24], // Rows
          [0, 5, 10,15,20], [1, 6, 11, 16,21], [2, 7, 12, 17, 22], [3, 8, 13, 18, 23], [4, 9, 14, 19, 24], // Columns
@@ -115,6 +114,7 @@
          
      ];
 
+    //check all five combinations
      for (const combo of winningCombinations) {
          const [a, b, c, d, e] = combo;
          
@@ -150,9 +150,7 @@
      console.log('Clear board');
  }
 
-
-
- // Function to update the player's turn
+ // Function to update the player's turn 
  function updatePlayerTurn() {
      const playerTurnElement = document.getElementById('player-turn');
      playerTurnElement.textContent = currentPlayer;
